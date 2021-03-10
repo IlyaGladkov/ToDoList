beac2eb41bd0b2bf9af385bcc037657c1d27e7b6
Iliya Gladkov <glagkov.iliya@gmail.com>
2021-03-10 21:39:25 +0300

    save data after reload

--- main.js
+++ main.js
@@ -5,21 +5,37 @@ let select = document.querySelector('.categories');
 let textArea = document.querySelector('.pullText');
 
 let buttonClick = document.querySelector('.addButton');
-buttonClick.addEventListener('click', () => {
-    let selectedCategory = select.options[select.selectedIndex].value;
-    let selectedDate = inputDate.value.split('-').splice(1,2).reverse().join('.');
-    let text = textArea.value;
-    let masHelp = [selectedCategory, text, selectedDate];
+let arrStorage = [];
 
+function createNewThing(needData) {
     let tr = document.createElement('tr');
     for (let i = 0; i < 3; i++) {
         let td = document.createElement('td');
-        td.textContent = masHelp[i];
+        td.textContent = needData[i];
         tr.append(td);
     }
     tr.childNodes[1].className = 'text';
     let table = document.querySelector('.list');
     table.append(tr); 
+}
 
+buttonClick.addEventListener('click', () => {
+    let selectedCategory = select.options[select.selectedIndex].value;
+    let selectedDate = inputDate.value.split('-').splice(1,2).reverse().join('.');
+    let text = textArea.value;
+    let masHelp = [selectedCategory, text, selectedDate];
+    arrStorage.push(masHelp);
+
+    localStorage.setItem('tasks', JSON.stringify(arrStorage));
+
+    createNewThing(masHelp);
+    
     textArea.value = '';
 })
+
+if (localStorage.getItem('tasks') != undefined) {
+    let backFromStorage = JSON.parse(localStorage.getItem('tasks'));
+    backFromStorage.forEach(ToDoElement => {
+        createNewThing(ToDoElement);
+    });
+}
